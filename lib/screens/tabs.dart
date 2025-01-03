62a8ae2566e0226922e1ec5598133c9409880834
vidit0018 <vidit.msit@gmail.com>
2025-01-03 16:06:09 +0530

    info messaging added

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
@@ -15,12 +16,28 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals =[];
+
+  void _showInfoMessage(String message){
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message))
+    );
+  }
+
   void _toggleMealFavoriteStatus(Meal meal){
     final isExisting = _favoriteMeals.contains(meal);
     if(isExisting){
+      setState(() {
+        
       _favoriteMeals.remove(meal);
+      _showInfoMessage('Meal removed from favorite');
+      });
     }else{
+      setState(() {
+        
       _favoriteMeals.add(meal);
+      _showInfoMessage('Meal marked as favorite');
+      });
     }
   }
   void _selectPage(int index) {
