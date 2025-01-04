c1fe1a91ee074720c7c150f6684ad54c59baad62
vidit0018 <vidit.msit@gmail.com>
2025-01-04 12:15:37 +0530

    drawer added

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -4,6 +4,7 @@ import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -46,6 +47,14 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _setScreen(String indentifier){
+    if(indentifier == 'filters'){
+
+    }else{
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget activePage =  CategoriesScreen(onToggleFavorite: _toggleMealFavoriteStatus,);
@@ -59,6 +68,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(activepageTitle),
       ),
+      drawer: MainDrawer(onSelectScreen: _setScreen),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(onTap: _selectPage, 
       currentIndex: _selectedPageIndex,
